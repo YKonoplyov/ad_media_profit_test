b767d766efbc43645d488c7573959a46b8e9ae58
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 17:46:28 +0300

    added GetSpendStatistic to spend app urls

--- spend/urls.py
+++ spend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from spend.views import SpendStatisticViewSet
+from spend.views import SpendStatisticViewSet, GetSpendStatistic
 from rest_framework.routers import DefaultRouter
 
 spend_router = DefaultRouter()
@@ -8,6 +8,7 @@ spend_router = DefaultRouter()
 spend_router.register("spend", SpendStatisticViewSet, basename="spend")
 
 urlpatterns = [
+    path('statistic/', GetSpendStatistic.as_view(), name="revenue-statistic"),
     path("spend/", include(spend_router.urls)),
 ]
 
