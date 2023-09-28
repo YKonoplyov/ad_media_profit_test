15a38cabe3ffa1b0a1247219e8841378913d5b9c
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 17:50:26 +0300

    added revenue app urls to global urls

--- spend_revenue_config/urls.py
+++ spend_revenue_config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("spend.urls", namespace="spend")),
+    path("api/v1/", include("revenue.urls", namespace="revenue"))
 ]
