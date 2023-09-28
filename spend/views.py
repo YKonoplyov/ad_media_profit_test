2086e2fc673afe77409db9e040d6c54aed4b6049
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 19:00:01 +0300

    code style improvement

--- spend/views.py
+++ spend/views.py
@@ -6,7 +6,10 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from spend.models import SpendStatistic
-from spend.serializers import SpendStatisticModelSerializer, SpendStatisticSerializer
+from spend.serializers import (
+    SpendStatisticModelSerializer,
+    SpendStatisticSerializer,
+)
 
 
 class GetSpendStatistic(APIView):
@@ -15,23 +18,25 @@ class GetSpendStatistic(APIView):
             return SpendStatisticSerializer
 
     def get_queryset(self):
-        queryset = SpendStatistic.objects.values("name", "date").annotate(
-            spend_sum=Sum("spend"),
-            impressions_sum=Sum("impressions"),
-            clicks_sum=Sum("clicks"),
-            conversion_sum=Sum("conversion"),
-            revenue_sum=Sum("revenue"),
-        ).annotate(
-            revenue_sum=Coalesce("revenue_sum", 0),
+        queryset = (
+            SpendStatistic.objects.values("name", "date")
+            .annotate(
+                spend_sum=Sum("spend"),
+                impressions_sum=Sum("impressions"),
+                clicks_sum=Sum("clicks"),
+                conversion_sum=Sum("conversion"),
+                revenue_sum=Sum("revenue"),
+            )
+            .annotate(
+                revenue_sum=Coalesce("revenue_sum", 0),
+            )
         )
         return queryset
 
     def get(self, request):
         queryset_data = list(self.get_queryset())
-        print(queryset_data)
         serializer_class = self.get_serializer_class(request)
         serializer = serializer_class(data=queryset_data, many=True)
-        print(serializer)
         if serializer.is_valid():
             return Response(serializer.data)
 
