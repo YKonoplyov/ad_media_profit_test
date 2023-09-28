a0aeea83e7b6909bc50e3799127669293f39b5f1
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 17:42:42 +0300

    created SpendStatisticModelViewSet

--- spend/views.py
+++ spend/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+
+from spend.models import SpendStatistic
+from spend.serializers import SpendStatisticModelSerializer, SpendStatisticSerializer
+
 
 # Create your views here.
+
+class SpendStatisticViewSet(ModelViewSet):
+    serializer_class = SpendStatisticModelSerializer
+    queryset = SpendStatistic.objects.all()
