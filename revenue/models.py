e02113a94e7020833691f70e558a6e659ca9a227
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 17:41:16 +0300

    added related name for RevenueStatistic model "spend" field

--- revenue/models.py
+++ revenue/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class RevenueStatistic(models.Model):
     name = models.CharField(max_length=255)
-    spend = models.ForeignKey('spend.SpendStatistic',
-                              on_delete=models.SET_NULL, null=True)
+    spend = models.ForeignKey(
+        'spend.SpendStatistic',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="revenue"
+    )
     date = models.DateField()
     revenue = models.DecimalField(max_digits=9, decimal_places=2, default=0)
