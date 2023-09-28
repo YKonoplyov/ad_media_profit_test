1aa7da52e2c16d5b050d90597a44a986ec48cb61
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 17:45:25 +0300

    created SpendStatisticSerializer

--- spend/serializers.py
+++ spend/serializers.py
@@ -8,3 +8,13 @@ class SpendStatisticModelSerializer(ModelSerializer):
     class Meta:
         model = SpendStatistic
         fields = "__all__"
+
+
+class SpendStatisticSerializer(Serializer):
+    name = serializers.CharField(max_length=255)
+    date = serializers.DateField()
+    spend_sum = serializers.DecimalField(max_digits=100, decimal_places=2)
+    impressions_sum = serializers.IntegerField()
+    clicks_sum = serializers.IntegerField()
+    conversion_sum = serializers.IntegerField()
+    revenue_sum = serializers.DecimalField(max_digits=100, decimal_places=2)
