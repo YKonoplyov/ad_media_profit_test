acfa22839e28098fa6a8d584d16f2c3b4feb25c6
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 18:08:04 +0300

    added GetRevenueStatistic view to revenue app urls

--- revenue/urls.py
+++ revenue/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from revenue.views import RevenueStatisticViewSet
+from revenue.views import RevenueStatisticViewSet, GetRevenueStatistic
 from rest_framework.routers import DefaultRouter
 
 revenue_router = DefaultRouter()
@@ -9,6 +9,11 @@ revenue_router.register("revenue", RevenueStatisticViewSet, basename="revenue")
 
 urlpatterns = [
     path("revenue/", include(revenue_router.urls)),
+    path(
+        "revenue_statistic/",
+        GetRevenueStatistic.as_view(),
+        name="revenue-statistic"
+    )
 ]
 
 app_name = "revenue"
