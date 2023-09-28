397d9d425c3ad1ed62c296b9bef2acd9d0601253
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-28 18:10:39 +0300

    installed django-debug-toolbar

--- spend_revenue_config/settings.py
+++ spend_revenue_config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "debug_toolbar",
+    "rest_framework",
+
+    "spend",
+    "revenue",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'spend_revenue_config.urls'
@@ -121,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- spend_revenue_config/urls.py
+++ spend_revenue_config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("spend.urls", namespace="spend")),
-    path("api/v1/", include("revenue.urls", namespace="revenue"))
+    path("api/v1/", include("revenue.urls", namespace="revenue")),
+    path("__debug__/", include("debug_toolbar.urls"))
 ]
